21492499649ac85616ee92279dcce2dafc15c011
Dev Aryan Dogra <devdogra1@gmail.com>
2023-08-26 21:01:01 +0530

    Add progress bar to TrainingCard

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import TrainingCard from "./Components/TrainingCard/TrainingCard";
 import { CardList } from "@blueprintjs/core";
+import classes from "./App.module.css"; 
 
 
 export default function App() {
@@ -13,11 +14,11 @@ export default function App() {
         textAlign: "center",
         fontWeight: "bold",
         margin: "1em 0"
-      }}>Available Trainings</h1>
+      }}>Your Trainings</h1>
       <ul className="training-card-list">
-        <TrainingCard className/>
-        <TrainingCard/>
-        <TrainingCard/>
+        <TrainingCard className={classes.trainingCard}/>
+        <TrainingCard className={classes.trainingCard}/>
+        <TrainingCard className={classes.trainingCard}/>
       </ul>
     </>
   );
--- src/Components/TrainingCard/TrainingCard.jsx
+++ src/Components/TrainingCard/TrainingCard.jsx
@@ -1,12 +1,19 @@
-import { Button, Card, Elevation } from "@blueprintjs/core";
+import { Button, Card, Elevation, ProgressBar } from "@blueprintjs/core";
+import { Intent } from "@blueprintjs/core";
+import classes from "./TrainingCard.module.css";
 
 
 export default function TrainingCard(props) {
     return (
         <Card className={props.className}interactive={true} elevation={Elevation.TWO}>
-            <h5><a href="#">Maggi Making</a></h5>
+            <h3><a href="#">Maggi Making</a></h3>
             <p>In this training you will learn how to make maggi</p>
-            <Button>Start</Button>
+
+            <div className={classes.progressContainer}>
+                <p>Progress</p>
+                <ProgressBar intent={Intent.PRIMARY} animate={false} stripes={false} value={0.4} className={classes.progressBar}/>
+                <Button className={classes.trainingResumeBtn} intent={Intent.SUCCESS}>Resume</Button>
+            </div>
         </Card>
     );
 }
